e418521ac87e2eb0161460951aeec26a3fd9d593
Abhinav <abhinavtb@gmail.com>
2024-04-06 18:30:33 -0500

    added modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,27 @@ import logo from './img/logo.png';
 
 
 // Modal component
-const Modal = ({ onClose }) => {
 
-  // function to alrert on click
+const Modal = ({ onClose }) => {
+  // function to alert on click
   const handleClick = () => {
     alert('Donated!');
-  }
+  };
+
   return (
     <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <p>Location: location 1</p>
-        <p>Contact: contact 1</p>
-        {/* Add features */}
-        <button onClick={onClose}>Close</button>
-        <button onClick={handleClick}>Donate</button> {/* Donate button */}
+      <div className="modal">
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <h1>Title 1</h1>
+          <p>Description: description 1</p>
+          <p>Location: location 1</p>
+          <p>Contact: contact 1</p>
+          {/* Add features */}
+          <div className="buttons">
+            <button className="close-btn" onClick={onClose}>Close</button>
+            <button className="donate-btn" onClick={handleClick}>Donate</button> {/* Donate button */}
+          </div>
+        </div>
       </div>
     </div>
   );
